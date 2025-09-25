b2125accbc85a48e097755d8a519194b82ed9df4
Darchi Axem <axem.dch4@gmail.com>
2025-09-25 17:24:43 +0200

    Système d'authentification avec gestion des droits fonctionnels (TP noté)

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -4,7 +4,7 @@ import { User } from "../models/user.model";
 
 export class AuthenticationService {
     public async authenticate(username: string, password: string): Promise<string> {
-        const user = await User.findOne({ where: { username, password } });
+        const user = await User.findOne({ where: { username, password }, raw: true });
 
         if(!user) {
             let error: CustomError = new Error("Invalid username or password");
@@ -12,9 +12,9 @@ export class AuthenticationService {
             throw error;
         }
 
-        console.log("Création du token pour l'utilisateur :", username)
+        console.log("Création d'un token pour l'utilisateur :", username)
         const token = jwt.sign(
-            { username },
+            { username: user.username },
             "your_secret_key",
             { expiresIn: '1h' });
         return token;
