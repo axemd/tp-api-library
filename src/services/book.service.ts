3805c23e3f053138ab7f8db90e0b93253e0f3a50
jdelcuserobert <jdelcuserobert@okteo.fr>
2025-09-25 11:35:06 +0200

    fix: Correction erreur correction route patch book

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,36 +1,43 @@
-import e from "express";
 import { CustomError } from "../middlewares/errorHandler";
 import { Author } from "../models/author.model";
 import { Book } from "../models/book.model";
 import { AuthorService } from "./author.service";
 
 export class BookService {
-
   public authorService = new AuthorService();
 
   public async getAllBooks(): Promise<Book[]> {
     return Book.findAll({
-        include: [{
-            model: Author,
-            as: 'author'
-        }]
-    });
-  }
-
-  public async getBookById(id: number): Promise<Book | null > {
-    return Book.findByPk(id, {
       include: [
         {
           model: Author,
-          as: 'author',
+          as: "author",
         },
       ],
     });
   }
 
-  public async createBook(title: string, publishYear: number, authorId: number, isbn: string): Promise<Book> {
-    let author: Author | null = await this.authorService.getAuthorById(authorId);
-    if(author === null) {
+  public async getBookById(id: number): Promise<Book | null> {
+    return Book.findByPk(id, {
+      include: [
+        {
+          model: Author,
+          as: "author",
+        },
+      ],
+    });
+  }
+
+  public async createBook(
+    title: string,
+    publishYear: number,
+    authorId: number,
+    isbn: string
+  ): Promise<Book> {
+    let author: Author | null = await this.authorService.getAuthorById(
+      authorId
+    );
+    if (author === null) {
       let error: CustomError = new Error(`Author ${authorId} not found`);
       error.status = 404;
       throw error;
@@ -38,36 +45,43 @@ export class BookService {
     return Book.create({ title, publishYear, authorId, isbn });
   }
 
-  public async updateBook(id: number, title: string, publishYear: number, authorId: number, isbn: string): Promise<Book | null> {
+  public async updateBook(
+    id: number,
+    title: string,
+    publishYear: number,
+    authorId: number,
+    isbn: string
+  ): Promise<Book> {
     let book = await this.getBookById(id);
-    if(book === null) {
+    if (book === null) {
       // Cette erreur pourrait être levée directement dans le contrôleur pour garder une cohérence de code
-      // Possibilité de gérer les erreurs dans le contrôleur ou le service selon les choix de développement
+      // Possibilité de gérer les erreurs dans le contrôleur ou le service selon les choix de
       let error: CustomError = new Error(`Book ${id} not found`);
       error.status = 404;
       throw error;
     } else {
-      if(authorId !== undefined) {
+      if (authorId !== undefined) {
         let author = await this.authorService.getAuthorById(authorId);
-        if(author === null) {
+        if (author === null) {
           let error: CustomError = new Error(`Author ${authorId} not found`);
           error.status = 404;
           throw error;
         }
       }
 
-      if(title !== undefined) {
+      if (title !== undefined) {
         book.title = title;
       }
 
-      if(publishYear !== undefined) {
+      if (publishYear !== undefined) {
         book.publishYear = publishYear;
-      } 
+      }
 
-      if(isbn !== undefined) {
+      if (isbn !== undefined) {
         book.isbn = isbn;
       }
       return book.save();
+    }
   }
 }
 
